26a45a1dc27e5fa95c721fe16462b5d523606adb
danielpowell4 <danielpowell12@msn.com>
2016-05-20 16:21:27 -0600

    rebuild workout generator

--- app/assets/javascripts/workout_gen.js
+++ app/assets/javascripts/workout_gen.js
@@ -15,8 +15,8 @@ var viewModel = {};
 var selectedIntensity = ko.observable(7);
 
     $( "#intensitySlider" ).slider({
-     min: 1,
-     max: 10,
+     min: 4,
+     max: 9,
      value: 7,
      change: function( event, ui ) {
        selectedIntensity(ui.value);
@@ -30,22 +30,24 @@ var todaysWorkout = ko.observable(0); // default hidden
 
 // the magic
 
-    var todaysFifteenWorkout = ko.observableArray(); // initalize active workout holder
-    var todaysFirstCircuit = ko.observableArray();
-    var todaysSecondCircuit = ko.observableArray();
-    var todaysThirdCircuit = ko.observableArray();
-    var todaysWarmUp = ko.observableArray();
-    var todaysCoolDown = ko.observableArray();
+    var firstCircuit = ko.observableArray();
+    var secondCircuit = ko.observableArray();
+    var thirdCircuit = ko.observableArray();
 
+    /* -- the categories -- */
+    var limitedExerciseCats = ['plyo','push','pull','core'];
+    var allExerciseCats = ['plyo','push','pull','core','legs'];
 
     /* -- verbose intensity filter -- */
 
     var filteredIntensity = ko.observableArray();
 
-    function filterByIntensity(){
+    function filterByIntensity(correction){
         filteredIntensity.removeAll(); // clear previous filter
 
-        var intensityRange = [selectedIntensity()-1,selectedIntensity(),selectedIntensity()+1];
+        correctedIntensity = selectedIntensity() + correction;
+
+        var intensityRange = [correctedIntensity-1,correctedIntensity,correctedIntensity+1];
         var justLess = _.where(EXERCISES, {"intensity": intensityRange[0]});
         var same = _.where(EXERCISES, {"intensity": intensityRange[1]});
         var justMore = _.where(EXERCISES, {"intensity": intensityRange[2]});
@@ -64,129 +66,58 @@ var todaysWorkout = ko.observable(0); // default hidden
 
     /* -- end intensity Filter -- */
 
-    /* -- assign warmUp, coolDown -- */
+    /* -- clear all workout circuits -- */
+    function clearCircuits(){
+      firstCircuit.removeAll();
+      secondCircuit.removeAll();
+      thirdCircuit.removeAll();
+    }
 
-    function generateWarmUpCoolDown(){
+    /* -- generate a circuit based on passed in categories -- */
 
-      todaysWarmUp.removeAll();
-      todaysCoolDown.removeAll();
-
-      filteredWarmUps = _.where(EXERCISES, {category: 'warmUp'});
-      var randomWarmUp = _.sample(filteredWarmUps);
-      todaysWarmUp.push(randomWarmUp);
-
-      filteredCoolDowns = _.where(EXERCISES, {category: 'coolDown'});
-      var randomCoolDown = _.sample(filteredCoolDowns);
-      todaysCoolDown.push(randomCoolDown);
-
-      }
-
-
-
-    /* -- assign ko.observables needed for 15 min workout -- */
-    function generateFifteen() {
-
-      filterByIntensity();
-      todaysFifteenWorkout.removeAll(); // clear todaysWorkout
-
-      /* set up needed categories */
-      var catsForFifteen = ['plyo','push','pull','core'];
-      var catsForFifteenLength = catsForFifteen.length;
+    function generateCircuit(circuitArray, exerciseCategories, intensityCorrection) {
+      filterByIntensity(intensityCorrection);
 
       /** --  loop through needed categories and
-              push a filtered sample into todaysWorkout -- **/
+              push a filtered sample into firstCircuit -- **/
 
-      for(var i=0; i<catsForFifteenLength; i++){
-        var filteredList = _.where(filteredIntensity(), {category: catsForFifteen[i]});
+      jQuery(exerciseCategories).each(function(index){
+        var filteredList = _.where(filteredIntensity(), {category: exerciseCategories[index]});
         var randomExercise = _.sample(filteredList);
-        todaysFifteenWorkout.push(randomExercise);
-      }
+        circuitArray.push(randomExercise);
+      });
 
-      todaysFifteenWorkout.sort(function() { return 0.5 - Math.random() });
-
-
+      /* -- randomize order -- */
+      circuitArray.sort(function() { return 0.5 - Math.random() });
     }
 
-    /* assign variables needed for 45 min */
-    function generateFourtyFive(){
-
-      filterByIntensity();
-
-      todaysFirstCircuit.removeAll();   // clearWorkout
-      todaysSecondCircuit.removeAll();  // clearWorkout
-
-        /* set up needed categories */
-        var catsForFourtyFive = ['plyo','push','pull','core','legs'];
-        var catsForFourtyFiveLength = catsForFourtyFive.length;
-
-        /** --  loop through needed categories and
-                push a filtered sample into todaysWorkout -- **/
-
-        for(var i=0; i<catsForFourtyFiveLength; i++){
-          var filteredList = _.where(filteredIntensity(), {category: catsForFourtyFive[i]});
-          var randomExercise = _.sample(filteredList);
-          todaysFirstCircuit.push(randomExercise);
-        }
-
-        for(var i=0; i<catsForFourtyFiveLength; i++){
-          var filteredList = _.where(filteredIntensity(), {category: catsForFourtyFive[i]});
-          var randomExercise = _.sample(filteredList);
-          todaysSecondCircuit.push(randomExercise);
-        }
-
-        /* randomize order of the workouts */
-        todaysFirstCircuit.sort(function() { return 0.5 - Math.random() });
-        todaysSecondCircuit.sort(function() { return 0.5 - Math.random() });
-
-    }
-
-    function generateSixty(){
-
-      filterByIntensity();
-
-      todaysFirstCircuit.removeAll();   // clearWorkout
-      todaysSecondCircuit.removeAll();  // clearWorkout
-      todaysThirdCircuit.removeAll();  // clearWorkout
-
-        /* set up needed categories */
-        var catsForSixty = ['plyo','push','pull','core','legs'];
-        var catsForSixtyLength = catsForSixty.length;
-
-        /** --  loop through needed categories and
-                push a filtered sample into todaysWorkout -- **/
-
-        // First Circuit
-        for(var i=0; i<catsForSixtyLength; i++){
-          var filteredList = _.where(filteredIntensity(), {category: catsForSixty[i]});
-          var randomExercise = _.sample(filteredList);
-          todaysFirstCircuit.push(randomExercise);
-        }
-        // Second Circuit
-        for(var i=0; i<catsForSixtyLength; i++){
-          var filteredList = _.where(filteredIntensity(), {category: catsForSixty[i]});
-          var randomExercise = _.sample(filteredList);
-          todaysSecondCircuit.push(randomExercise);
-        }
-        // Third Circuit
-        for(var i=0; i<catsForSixtyLength; i++){
-          var filteredList = _.where(filteredIntensity(), {category: catsForSixty[i]});
-          var randomExercise = _.sample(filteredList);
-          todaysThirdCircuit.push(randomExercise);
-        }
-
-        /* randomize order of the workouts */
-        todaysFirstCircuit.sort(function() { return 0.5 - Math.random() });
-        todaysSecondCircuit.sort(function() { return 0.5 - Math.random() });
-        todaysThirdCircuit.sort(function() { return 0.5 - Math.random() });
-
-    }
+/* -- when the generate button is clicked -- */
 
 $('#generate').click(function(){
   todaysWorkout(0);
-  generateWarmUpCoolDown();
-  generateFifteen();
-  generateFourtyFive();
-  generateSixty();
+  clearCircuits();
+
+  switch (selectedWorkout()) {
+    case 1:
+      generateCircuit(firstCircuit(), limitedExerciseCats, 0);
+      break;
+    case 2:
+      generateCircuit(firstCircuit(), allExerciseCats, -1);
+      generateCircuit(secondCircuit(), allExerciseCats, 0);
+      break;
+    case 3:
+      generateCircuit(firstCircuit(), allExerciseCats, 0);
+      generateCircuit(secondCircuit(), allExerciseCats, 1);
+      generateCircuit(thirdCircuit(), allExerciseCats, -1);
+      console.log(firstCircuit()[1].intensity);
+      console.log(secondCircuit()[1].intensity);
+      console.log(thirdCircuit()[1].intensity);
+      console.log('---');
+      break;
+    default:
+      alert('Please select how many circuits you would like to do.')
+  }
+
   todaysWorkout(1);
 
 });
